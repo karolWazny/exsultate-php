a3868f1a15305c38d6d4c5ad53a478e3e78fcdf2
karolWazny <karoljanwazny@gmail.com>
2022-02-10 13:58:18 +0100

    CPT data as static member instead of locally declared variable

--- classes/cpt/class-cpt-song.php
+++ classes/cpt/class-cpt-song.php
@@ -4,25 +4,24 @@ class ExsultateCustomPostTypeSong
 {
     private static $_instance = null;
 
+    private static $_cpt_data = [
+        'labels' => [
+            'name' =>  'Songs' ,
+            'singular_name' => 'Song'
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => ['slug' => 'songs'],
+        'show_in_rest' => false,
+
+    ];
+
     private function __construct(){
         add_action( 'init', array($this, 'create_post_type' ));
     }
 
     public function create_post_type(){
-        register_post_type( 'songs',
-            // CPT Options
-            array(
-                'labels' => array(
-                    'name' => __( 'Songs' ),
-                    'singular_name' => __( 'Song' )
-                ),
-                'public' => true,
-                'has_archive' => true,
-                'rewrite' => array('slug' => 'songs'),
-                'show_in_rest' => false,
-
-            )
-        );
+        register_post_type( 'songs', self::$_cpt_data);
     }
 
     public static function instance(){
