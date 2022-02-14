e89966572e744580adcfe02f1868889dada57e27
karolWazny <karoljanwazny@gmail.com>
2022-02-14 17:07:59 +0100

    started working on ExsultateSongDisplayFormatter

--- classes/cpt/class-song-display-formatter.php
+++ classes/cpt/class-song-display-formatter.php
@@ -2,8 +2,20 @@
 
 class ExsultateSongDisplayFormatter
 {
+    private $post_id;
+    private $content;
+
     public function format( &$raw_content, $post_id) {
-        $raw_content = $raw_content . 'some test content';
+        $this->post_id = $post_id;
+        $this->content = $raw_content;
+
+        $this->do_magic();
+
+        $raw_content = $this->content;
         return $raw_content;
+    }
+
+    private function do_magic(){
+
     }
 }
